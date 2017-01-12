366e783f016e5dadcc933fa9292bc7f28c9572fb
Jorge Aparicio <japaricious@gmail.com>
2017-01-12 16:06:04 -0500

    tweak platform/linux-x86_64
    to match the formatting of the other modules

--- src/platform/linux-x86_64/mod.rs
+++ src/platform/linux-x86_64/mod.rs
@@ -12,80 +12,78 @@
 pub mod nr;
 
 #[inline(always)]
-pub unsafe fn syscall0(n: usize) -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+pub unsafe fn syscall0(mut n: usize) -> usize {
+    asm!("syscall"
+         : "+{rax}"(n)
+         :
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
 
 #[inline(always)]
-pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n), "{rdi}"(a1)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+pub unsafe fn syscall1(mut n: usize, a1: usize) -> usize {
+    asm!("syscall"
+         : "+{rax}"(n)
+         : "{rdi}"(a1)
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
 
 #[inline(always)]
-pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+pub unsafe fn syscall2(mut n: usize, a1: usize, a2: usize) -> usize {
+    asm!("syscall"
+         : "+{rax}"(n)
+         : "{rdi}"(a1) "{rsi}"(a2)
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
 
 #[inline(always)]
-pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2), "{rdx}"(a3)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+pub unsafe fn syscall3(mut n: usize, a1: usize, a2: usize, a3: usize) -> usize {
+    asm!("syscall"
+         : "+{rax}"(n)
+         : "{rdi}"(a1) "{rsi}"(a2) "{rdx}"(a3)
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
 
 #[inline(always)]
-pub unsafe fn syscall4(n: usize,
+pub unsafe fn syscall4(mut n: usize,
                        a1: usize,
                        a2: usize,
                        a3: usize,
                        a4: usize)
                        -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2), "{rdx}"(a3),
-                     "{r10}"(a4)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+    asm!("syscall"
+         : "+{rax}"(n)
+         : "{rdi}"(a1) "{rsi}"(a2) "{rdx}"(a3) "{r10}"(a4)
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
 
 #[inline(always)]
-pub unsafe fn syscall5(n: usize,
+pub unsafe fn syscall5(mut n: usize,
                        a1: usize,
                        a2: usize,
                        a3: usize,
                        a4: usize,
                        a5: usize)
                        -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2), "{rdx}"(a3),
-                     "{r10}"(a4), "{r8}"(a5)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+    asm!("syscall"
+         : "+{rax}"(n)
+         : "{rdi}"(a1) "{rsi}"(a2) "{rdx}"(a3) "{r10}"(a4) "{r8}"(a5)
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
 
 #[inline(always)]
-pub unsafe fn syscall6(n: usize,
+pub unsafe fn syscall6(mut n: usize,
                        a1: usize,
                        a2: usize,
                        a3: usize,
@@ -93,11 +91,10 @@ pub unsafe fn syscall6(n: usize,
                        a5: usize,
                        a6: usize)
                        -> usize {
-    let ret: usize;
-    asm!("syscall" : "={rax}"(ret)
-                   : "{rax}"(n), "{rdi}"(a1), "{rsi}"(a2), "{rdx}"(a3),
-                     "{r10}"(a4), "{r8}"(a5), "{r9}"(a6)
-                   : "rcx", "r11", "memory"
-                   : "volatile");
-    ret
+    asm!("syscall"
+         : "+{rax}"(n)
+         : "{rdi}"(a1) "{rsi}"(a2) "{rdx}"(a3) "{r10}"(a4) "{r8}"(a5)"{r9}"(a6)
+         : "rcx", "r11", "memory"
+         : "volatile");
+    n
 }
