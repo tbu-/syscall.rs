21f8492e671408c76ed402c3158927583c1e9ef5
Jorge Aparicio <japaricious@gmail.com>
2017-01-12 16:04:27 -0500

    tweak platform/linux-armeabi
    to match the formatting of the other modules

--- src/platform/linux-armeabi/mod.rs
+++ src/platform/linux-armeabi/mod.rs
@@ -14,40 +14,44 @@ pub mod nr;
 #[inline(always)]
 pub unsafe fn syscall0(n: usize) -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall1(n: usize, a1: usize) -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n) "{r0}"(a1)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall2(n: usize, a1: usize, a2: usize) -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1), "{r1}"(a2)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n) "{r0}"(a1) "{r1}"(a2)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
 #[inline(always)]
 pub unsafe fn syscall3(n: usize, a1: usize, a2: usize, a3: usize) -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1), "{r1}"(a2), "{r2}"(a3)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
@@ -59,11 +63,11 @@ pub unsafe fn syscall4(n: usize,
                        a4: usize)
                        -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1), "{r1}"(a2), "{r2}"(a3),
-                     "{r3}"(a4)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
@@ -77,10 +81,9 @@ pub unsafe fn syscall5(n: usize,
                        -> usize {
     let ret: usize;
     asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1), "{r1}"(a2), "{r2}"(a3),
-                     "{r3}"(a4), "{r4}"(a5)
-                   : "memory" "cc"
-                   : "volatile");
+         : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4) "{r4}"(a5)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
@@ -94,11 +97,12 @@ pub unsafe fn syscall6(n: usize,
                        a6: usize)
                        -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1), "{r1}"(a2), "{r2}"(a3),
-                     "{r3}"(a4), "{r4}"(a5), "{r5}"(a6)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4) "{r4}"(a5)
+           "{r5}"(a6)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
 
@@ -113,10 +117,11 @@ pub unsafe fn syscall7(n: usize,
                        a7: usize)
                        -> usize {
     let ret: usize;
-    asm!("swi $$0" : "={r0}"(ret)
-                   : "{r7}"(n), "{r0}"(a1), "{r1}"(a2), "{r2}"(a3),
-                     "{r3}"(a4), "{r4}"(a5), "{r5}"(a6), "{r6}"(a7)
-                   : "memory" "cc"
-                   : "volatile");
+    asm!("swi $$0"
+         : "={r0}"(ret)
+         : "{r7}"(n) "{r0}"(a1) "{r1}"(a2) "{r2}"(a3) "{r3}"(a4) "{r4}"(a5)
+           "{r5}"(a6) "{r6}"(a7)
+         : "memory" "cc"
+         : "volatile");
     ret
 }
