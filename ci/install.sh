42b5a4ead770705fcf06b2d35233070272cdab72
Jorge Aparicio <japaricious@gmail.com>
2017-01-11 21:08:11 -0500

    fix ci/install.sh for macOS

--- ci/install.sh
+++ ci/install.sh
@@ -4,12 +4,19 @@ main() {
     curl https://sh.rustup.rs -sSf | \
         sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
 
+    local target=
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        target=x86_64-unknown-linux-gnu
+    else
+        target=x86_64-apple-darwin
+    fi
+
     curl -LSfs https://japaric.github.io/trust/install.sh | \
         sh -s -- \
            --force \
            --git japaric/cross \
            --tag v0.1.4 \
-           --target x86_64-unknown-linux-gnu
+           --target $target
 }
 
 main
